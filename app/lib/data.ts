6a3079bd30f59d5f16918fa6e52ee19a0612954c
Paolo Donadeo <paolo@donadeo.net>
2024-01-17 18:12:35 +0100

    Finito capitolo 9: https://nextjs.org/learn/dashboard-app/streaming#practice-streaming-latestinvoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -38,6 +38,8 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -60,6 +62,8 @@ export async function fetchCardData() {
   noStore();
 
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.

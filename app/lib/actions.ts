212e0fe200e6672a97de2c480078a94388dbdfc6
Paolo Donadeo <paolo@donadeo.net>
2024-01-24 15:51:08 +0100

    Completato capitolo 15: https://nextjs.org/learn/dashboard-app/adding-authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { sql } from '@vercel/postgres';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -108,5 +111,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: `Error: Failed to Delete Invoice.`,
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
